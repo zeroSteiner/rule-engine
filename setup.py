5be97af883b72d8c901790028b6093e569f415a7
Spencer McIntyre <zeroSteiner@gmail.com>
2018-04-30 19:36:27 -0400

    Update .gitignore and setup.py for a beta release

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ setup(
 	package_dir={'': 'lib'},
 	packages=find_packages('lib'),
 	classifiers=[
-		'Development Status :: 5 - Production/Stable',
+		'Development Status :: 4 - Beta',
 		'Environment :: Console',
 		'Intended Audience :: Developers',
 		'License :: OSI Approved :: BSD License',

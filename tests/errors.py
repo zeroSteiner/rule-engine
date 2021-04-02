3cf4b43daf89efe2373e46c4e76c3c520a8fb845
Spencer McIntyre <zeroSteiner@gmail.com>
2021-04-01 21:56:46 -0400

    Add tests for the repr on resolution errors

--- tests/errors.py
+++ tests/errors.py
@@ -30,9 +30,30 @@
 #  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
+import random
+import string
 import unittest
 
 import rule_engine.errors as errors
+
+class ResolutionErrorTests(unittest.TestCase):
+	def test_attribute_error_repr(self):
+		attribute_error = errors.AttributeResolutionError('doesnotexist', None)
+		self.assertIn('suggestion', repr(attribute_error))
+
+		suggestion = ''.join(random.choice(string.ascii_letters) for _ in range(10))
+		attribute_error = errors.AttributeResolutionError('doesnotexist', None, suggestion=suggestion)
+		self.assertIn('suggestion', repr(attribute_error))
+		self.assertIn(suggestion, repr(attribute_error))
+
+	def test_symbol_error_repr(self):
+		symbol_error = errors.SymbolResolutionError('doesnotexist')
+		self.assertIn('suggestion', repr(symbol_error))
+
+		suggestion = ''.join(random.choice(string.ascii_letters) for _ in range(10))
+		symbol_error = errors.SymbolResolutionError('doesnotexist', suggestion=suggestion)
+		self.assertIn('suggestion', repr(symbol_error))
+		self.assertIn(suggestion, repr(symbol_error))
 
 class UndefinedSentinelTests(unittest.TestCase):
 	def test_undefined_has_a_repr(self):

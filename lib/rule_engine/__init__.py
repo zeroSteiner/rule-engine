c3d06053101b87fdf3aab1591dbb64f2d53fc53d
Spencer McIntyre <zeroSteiner@gmail.com>
2026-06-25 13:13:45 -0400

    Bump the version to 5.0.2

--- lib/rule_engine/__init__.py
+++ lib/rule_engine/__init__.py
@@ -30,7 +30,7 @@
 #  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-__version__ = '5.0.1'
+__version__ = '5.0.2'
 
 from .engine import resolve_attribute
 from .engine import resolve_item
